a05d0159847d4cd73ab60c1fc01c5c9025b21fc9
Yusuf-AI2011 <yusufshomurodov36@gmail.com>
2026-01-17 14:01:16 +0500

    changed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let api = `http://www.omdbapi.com/?s=shrek&apikey=3e658e50`;
 const search = document.querySelector(".search");
 const cards = document.querySelector(".cards");
+const loading = document.querySelector(".loading");
 
 try {
   getMovie(api);
@@ -9,15 +10,19 @@ try {
 }
 
 function getMovie(api) {
+  loading.innerHTML = "";
   cards.innerHTML = "";
   axios.get(api).then((data) => {
     console.log(data.data);
     const request = data.data;
     request.Search.map((item, index) => {
       cards.innerHTML += `
-    <img src=${item.Poster} alt="" class="image" />
+      <div class="card">
+      <img src=${item.Poster} alt="" class="image" />
     <h1 class="title">${item.Title}</h1>
     <p class="date">${item.Year}</p>
+      </div>
+    
   `;
     });
   });
